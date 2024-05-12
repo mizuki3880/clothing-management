10acc1a2a03c37ee80a5cdfb3bf67ad49f1612d6
mizuki3880 <115447795+mizuki3880@users.noreply.github.com>
2024-05-12 16:47:28 +0900

    パスワードリセット機能作成（Email送信）

--- front-end/actions/reset.ts
+++ front-end/actions/reset.ts
@@ -3,6 +3,8 @@
 import * as z from "zod";
 import { ResetSchema } from "../schemas";
 import { getUserByEmail } from "../data/user";
+import { sendPasswordResetEmail } from "@/lib/mail";
+import { generatePasswordResetToken } from "@/lib/token";
 
 export const reset = async (values: z.infer<typeof ResetSchema>) => {
   const validatedFields = ResetSchema.safeParse(values);
@@ -18,6 +20,12 @@ export const reset = async (values: z.infer<typeof ResetSchema>) => {
   if (!existingUser) {
     return { error: "メールアドレスが存在しません" };
   }
+
+  const passwordResetToken = await generatePasswordResetToken(email);
+  await sendPasswordResetEmail(
+    passwordResetToken.email,
+    passwordResetToken.token
+  );
 
   return { success: "リセットメールを送信しました！" };
 };
